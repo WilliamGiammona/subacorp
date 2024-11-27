ae333e795a4bd7af598274c88a58d122037b6d36
William Giammona <wgiammona@gmail.com>
2024-11-27 19:00:20 +0200

    navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
 export default function Home() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            {/* Add content here */}
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
