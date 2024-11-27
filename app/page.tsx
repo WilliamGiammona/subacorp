2e0142d3e1a8b24ae8aff86b756f877cea64aced
William Giammona <wgiammona@gmail.com>
2024-11-27 21:22:56 +0200

    added featured properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/ui/navigation/NavBar";
 import Hero from "./Hero";
+import FeaturedProperties from "./FeaturedProperties";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <NavBar />
       <Hero />
+      <FeaturedProperties />
     </div>
   );
 }
