670c9ce315807fdcf0a4e2c96f319b7ce2edf0bd
William Giammona <wgiammona@gmail.com>
2024-11-27 21:04:29 +0200

    name change

---
+++ app/properties/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/ui/navigation/NavBar";
+
+const properties = () => {
+  return;
+  <>
+    <div>
+      hi
+      <NavBar />
+    </div>
+  </>;
+};
+
+export default properties;
