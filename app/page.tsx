e1b7bf652eef9a53d61505cbadd181d936e8ba62
William Giammona <wgiammona@gmail.com>
2024-11-27 21:14:59 +0200

    added hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/ui/navigation/NavBar";
+import Hero from "./Hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <NavBar />
+      <Hero />
     </div>
   );
 }
