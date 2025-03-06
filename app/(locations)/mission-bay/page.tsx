36610a386c4233cdff7c37f1942d7d038d05c463
William Giammona <wgiammona@gmail.com>
2025-03-06 13:59:35 +0200

    updated description

--- app/(locations)/mission-bay/page.tsx
+++ app/(locations)/mission-bay/page.tsx
@@ -203,16 +203,18 @@ export default function MissionBay() {
             4501 Mission Bay Dr., San Diego, CA 92109
           </p>
           <p className="mt-8  text-gray-600 dark:text-gray-300 text-sm md:text-base">
-            Mission Bay Center is conveniently located in a high traffic/high
-            visibility location in the Mission Bay area of San Diego. This
-            center has easy access to highways 5 and 52 as well as ample parking
-            for customers, and currently has award winning restaurants, a
-            convenience store, and many medical and professional tenants. It is
-            near the Mission Bay Golf Course as well as the beach! The traffic
-            count is approximately 31,360 ADT. The median income is
-            approximately $71,000 with the average home price being $982,000.
-            The immediate surrounding population is 48,000 with a density of
-            6,250 people per square mile.
+            Mission Bay Professional Center is conveniently located in a high
+            traffic/high visibility location in the Mission Bay area of San
+            Diego. The building has easy access to Highways 5 and 52 as well as
+            ample parking for customers, and currently has award winning
+            restaurants, a convenience store, and many medical and professional
+            tenants. It is near the Mission Bay Golf Course as well as the
+            beach! The traffic count is approximately 31,360 ADT. The median
+            income is approximately $71,000 with the average home price being
+            $982,000. The immediate surrounding population is 48,000 with a
+            density of 6,250 people per square mile. Nearby _blank_ include many
+            fine dining restaurants, retail stores, Mission Bay, Mission Bay
+            Golf Course, and of course the beach!
             <br />
           </p>
         </div>
