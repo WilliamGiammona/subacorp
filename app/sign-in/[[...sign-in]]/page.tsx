99641c06474ddb803b65f4bb8594170e34888d5e
William Giammona <wgiammona@gmail.com>
2025-03-06 15:11:25 +0200

    refactor

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,12 @@
-import { SignIn } from "@clerk/nextjs";
+// import { SignIn } from "@clerk/nextjs";
 
-export default function SignInPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white">
-      <div className="max-w-md w-full p-8 rounded-lg">
-        <h1 className="text-3xl font-bold mb-8 text-center">SUBA CORP Admin</h1>
-        <SignIn />
-      </div>
-    </div>
-  );
-}
+// export default function SignInPage() {
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-black text-white">
+//       <div className="max-w-md w-full p-8 rounded-lg">
+//         <h1 className="text-3xl font-bold mb-8 text-center">SUBA CORP Admin</h1>
+//         <SignIn />
+//       </div>
+//     </div>
+//   );
+// }
