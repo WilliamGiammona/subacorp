9d4b4afa2597456bcda89d14f42bced6aa128a45
William Giammona <wgiammona@gmail.com>
2025-05-13 13:18:26 +0300

    updated location

--- app/(locations)/solana-beach/page.tsx
+++ app/(locations)/solana-beach/page.tsx
@@ -77,7 +77,7 @@ export default function SolanaBeach() {
       ],
       imageUrl: "/images/mission-bay/suite1L/Exterior1L.jpg",
       type: "Office Space",
-      location: "Mission Bay",
+      location: "Solana Beach",
       features: [
         { name: "Spacious Offices", status: "Included" },
         { name: "Waiting Area", status: "Included" },
